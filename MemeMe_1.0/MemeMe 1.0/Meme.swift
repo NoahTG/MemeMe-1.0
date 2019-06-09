f17a3f76d6f5c22915f7e005937907d33fb32f13
NoahTG <26232817+NoahTG@users.noreply.github.com>
2019-06-08 23:23:25 -0700

    Add files via upload

---
+++ MemeMe_1.0/MemeMe 1.0/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe 1.0
+//
+//  Created by NTG on 6/8/19.
+//  Copyright © 2019 NTG. All rights reserved.
+//
+
+import UIKit
+
+
+struct Meme {
+    let topText: String!
+    let bottomText: String!
+    let originalImage: UIImage!
+    let memedImage: UIImage!
+}
+
